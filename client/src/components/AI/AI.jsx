aa0256b3adfffd5219701e8bfa243d5828b50234
Pooya <jb.pooya@gmail.com>
2024-01-13 11:32:24 +0100

    add styles

--- client/src/components/AI/AI.jsx
+++ client/src/components/AI/AI.jsx
@@ -1,22 +1,26 @@
 import classes from './AI.module.css';
 import Question from './Question/Question';
 import ask from '../../assets/ASK.svg';
+import Answer from './Answer/Answer';
 
 const questions = [
-  'What is my current fitness level?',
-  'Suggest a post-workout meal',
+  'What is my current Fitness Level?',
+  'Suggest a post-workout meal!',
   'What is a healthy breakfast option for me?',
-  'Recommend a workout routine for me',
+  'Recommend a workout routine for me!',
 ];
 
 const AI = () => {
   return (
     <div className={classes.AI}>
-      <img className={classes.ask} src={ask} alt='ask AI picture' />
+      <img className={classes.askImg} src={ask} alt='ask AI picture' />
       <div className={classes.questions}>
         {questions.map((q, index) => (
           <Question question={q} key={index} />
         ))}
+      </div>
+      <div className={classes.answer}>
+        <Answer />
       </div>
     </div>
   );
---
+++ client/src/components/AI/Answer/Answer.jsx
@@ -0,0 +1,11 @@
+import classes from './Answer.module.css';
+
+const Answer = () => {
+  return (
+    <div className={classes.answer}>
+      <p>Answer from AI comes here!</p>
+    </div>
+  );
+};
+
+export default Answer;
